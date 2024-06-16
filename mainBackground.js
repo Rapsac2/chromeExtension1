eda81805083e1f4eb2e1ecdeb93c988b1daa772d
Rapsac <caspar.kersting@gmail.com>
2024-06-16 21:11:37 +0200

    fisrt day end

---
+++ mainBackground.js
@@ -0,0 +1,8 @@
+console.log("hat geladen");
+
+async function sendMessageToActiveTab(message) {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    const response = await chrome.tabs.sendMessage(tab.id, message);
+}
+
+sendMessageToActiveTab("hi");
